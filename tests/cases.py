54d16abbc7196c85a7651f0ad034bbf526851667
Arthur Noel <arthur@ischium.net>
2023-10-14 08:50:19 +0100

    testcase __str__ returns "test"

--- tests/cases.py
+++ tests/cases.py
@@ -6,4 +6,4 @@ from distinfo.base import Base
 class Case(Base):
     def __str__(self) -> str:  # pragma: no cover
         # required for __repr__, needed for debugging tests
-        return "test case"
+        return "test"

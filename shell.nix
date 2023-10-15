13e3fac7e5dab88e0ce1bc1adda3722c51f444f5
Arthur Noel <arthur@ischium.net>
2023-10-15 07:10:46 +0100

    use cachix in preShellHook

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-(import ./default.nix {}).overrideAttrs (old: {
-  nativeBuildInputs = old.nativeBuildInputs ++ [ cachix jq ];
+(import ./default.nix {}).overrideAttrs (self: {
+  nativeBuildInputs = self.nativeBuildInputs ++ [ cachix jq ];
+  preShellHook = ''
+    cachix use ${self.pname}
+    ${self.preShellHook}
+  '';
 })
